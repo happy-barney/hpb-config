c3687b3d602097d785c14cf34955a31145faf951
Branislav Zahradnik <happy.barney@gmail.com>
2016-04-19 10:43:21 +0200

    Do not remove module name while guessing puppet manifest name

--- emacs/local/share/lisp.d/hpb-puppet-guess-manifest-name.el
+++ emacs/local/share/lisp.d/hpb-puppet-guess-manifest-name.el
@@ -6,7 +6,7 @@
          (file (buffer-file-name (current-buffer)))
          (without-suffix    (replace-regexp-in-string "\\.pp\$" "" file))
          (without-modules   (replace-regexp-in-string ".*\\/modules\\/" "" without-suffix))
-         (without-manifests (replace-regexp-in-string ".*\\manifests\\/\\(init\\)?" "" without-modules))
+         (without-manifests (replace-regexp-in-string "manifests\\/\\(init\\)?" "" without-modules))
          (manifest-name     (replace-regexp-in-string "\\/" "::" without-manifests))
         )
     manifest-name
