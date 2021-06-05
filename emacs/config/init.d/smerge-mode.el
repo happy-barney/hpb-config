fe63c8204ac177449a1868dc0160bce9e11896dc
Branislav Zahradník <happy.barney@gmail.com>
2021-06-05 11:12:44 +0200

    Use new smerge-mode functions, bind keep-all

--- emacs/config/init.d/smerge-mode.el
+++ emacs/config/init.d/smerge-mode.el
@@ -5,5 +5,6 @@
 (define-key smerge-mode-map (kbd "<f7>") 'smerge-prev)
 (define-key smerge-mode-map (kbd "<f8>") 'smerge-next)
 
-(define-key smerge-mode-map (kbd "<f9>") 'smerge-keep-mine)
-(define-key smerge-mode-map (kbd "<f10>") 'smerge-keep-other)
+(define-key smerge-mode-map (kbd "<f9>") 'smerge-keep-upper)
+(define-key smerge-mode-map (kbd "<f10>") 'smerge-keep-lower)
+(define-key smerge-mode-map (kbd "<f11>") 'smerge-keep-all)
