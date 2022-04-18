4841533079af3d97e2d1326d137e2d939deab9ee
Branislav Zahradník <happy.barney@gmail.com>
2022-04-18 14:55:11 +0200

    hpb-perl - Adapt play/test file recognition according AAW

--- emacs/local/share/lisp.d/hpb-perl-find-related-file.el
+++ emacs/local/share/lisp.d/hpb-perl-find-related-file.el
@@ -3,6 +3,16 @@
 (require 'ert)
 (require 's)
 
+;(defun hpb-perl-find-related-file/guess-path (path)
+;  (cond
+;   ((hpb-perl-find-related-file/veure-looks-like-test-class path)
+;	(hpb-perl-find-related-file/veure-make-play-file
+;	 (projectile-project-root)
+;	 (string-match-p "/t/TestFor\\(/.*\\\\.pm)")
+;	 )
+;	(concat (projectile-project-root) "lib" (
+;(defun hpb-perl-find-related-file/looks-like-veure-test-class
+
 (defun hpb-perl-guess-test-or-implementation-path (path force-dash)
   (cond
    ((hpb-perl-looks-like-test-file-p path) (hpb-perl-make-play-file-path path))
@@ -13,8 +23,12 @@
 
 (defun hpb-perl-looks-like-test-file-p (path)
   "predicate: does it looks like test file?"
-  (string-match-p "/t/.*\\.t$" path)
-  )
+  (cond
+   ((string-match-p "/t/lib/"                    path) nil)
+   ((string-match-p "/t/tests/TestsFor/.*\\.pm$" path) t)
+   ((string-match-p "/t/.*\\.t$"                 path) t)
+   (t nil)
+  ))
 
 (defun hpb-perl-looks-like-play-file-p (path)
   (cond
@@ -24,23 +38,57 @@
    (t t)
    ))
 
-(defun hpb-perl-make-test-file-path (path force-dash)
+(defun hpb-perl-make-test-file-path (path &optional force-dash)
+  (hpb-perl-make-test-file-path/aaw path)
+    )
+
+(defun hpb-perl-make-test-file-path/aaw (path &optional force-dash)
   (let* ((parts (s-split "/lib/" path))
          (last-part (nth (+ -1 (safe-length parts)) parts))
          (path-with-ext (replace-regexp-in-string "\\.pm$" ".t" last-part))
          (path-with-dash (replace-regexp-in-string "/" "-" path-with-ext))
-         (prefix (concat (s-join "/lib/" (butlast parts)) "/t/"))
+         (prefix (concat (s-join "/lib/" (butlast parts)) "/t/tests/TestsFor/"))
+         (full-path-without-ext (concat prefix last-part))
+         (full-path-with-ext (concat prefix path-with-ext))
+         (full-path-with-dash (concat prefix path-with-dash))
+         )
+    (cond
+     ((file-exists-p full-path-without-ext) full-path-without-ext)
+     ((file-exists-p full-path-with-ext) full-path-with-ext)
+     (t full-path-with-dash)
+     )
+    ))
+
+(defun hpb-perl-make-test-file-path/gdc (path &optional force-dash)
+  (let* ((parts (s-split "/lib/" path))
+         (last-part (nth (+ -1 (safe-length parts)) parts))
+         (path-with-ext (replace-regexp-in-string "\\.pm$" ".t" last-part))
+         (path-with-dash (replace-regexp-in-string "/" "-" path-with-ext))
+         (prefix (concat (s-join "/lib/" (butlast parts)) "/t/tests/TestsFor/"))
+         (full-path-without-ext (concat prefix last-part))
          (full-path-with-ext (concat prefix path-with-ext))
          (full-path-with-dash (concat prefix path-with-dash))
          )
     (cond
      (force-dash full-path-with-dash)
+     ((file-exists-p full-path-without-ext) full-path-without-ext)
      ((file-exists-p full-path-with-ext) full-path-with-ext)
      (t full-path-with-dash)
      )
     ))
 
 (defun hpb-perl-make-play-file-path (path)
+  (hpb-perl-make-play-file-path/aaw path)
+    )
+
+(defun hpb-perl-make-play-file-path/aaw (path)
+  (let* ((parts (s-split "/t/tests/TestsFor/" path))
+         (last-part (nth (+ -1 (safe-length parts)) parts))
+         )
+    (concat (s-join "/lib/" (append (butlast parts) (list last-part))))
+    ))
+
+(defun hpb-perl-make-play-file-path/gdc (path)
   (let* ((parts (s-split "/t/" path))
          (last-part (nth (+ -1 (safe-length parts)) parts))
          (path-with-ext (replace-regexp-in-string "\\.t$" ".pm" last-part))
