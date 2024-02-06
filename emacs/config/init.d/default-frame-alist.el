670ea72409deee42c08e727a40370354f4e6938d
Branislav Zahradník <happy.barney@gmail.com>
2024-02-06 06:11:39 +0100

    [emacs/default-frame-alist.el] Tune default size for currently used monitors

--- emacs/config/init.d/default-frame-alist.el
+++ emacs/config/init.d/default-frame-alist.el
@@ -3,10 +3,15 @@
 (add-to-list 'default-frame-alist '(background-color . "#c0c0c0"))
 
 (cond
+ ((and (= (display-pixel-width) 1920) (= (display-pixel-height) 1200))
+  (add-to-list 'default-frame-alist '(width  . 92))
+  (add-to-list 'default-frame-alist '(height . 55)))
+
+ ((= (display-pixel-width) 1920)
+  (add-to-list 'default-frame-alist '(width  . 92))
+  (add-to-list 'default-frame-alist '(height . 41)))
+
  ((= (display-pixel-width) 1680)
   (add-to-list 'default-frame-alist '(width  . 89))
   (add-to-list 'default-frame-alist '(height . 41)))
- ((= (display-pixel-width) 1920)
-  (add-to-list 'default-frame-alist '(width  . 103))
-  (add-to-list 'default-frame-alist '(height . 42)))
 )
