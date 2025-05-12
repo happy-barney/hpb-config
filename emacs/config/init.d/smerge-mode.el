324c50856e2a093932cca99cb49d2729489d252b
Branislav Zahradník <happy.barney@gmail.com>
2025-05-12 22:04:12 +0200

    [emacs/smerge] hpb-smerge-extend

--- emacs/config/init.d/smerge-mode.el
+++ emacs/config/init.d/smerge-mode.el
@@ -1,6 +1,16 @@
 ;;; -*- Mode: Emacs-Lisp -*-
 
 (require 'smerge-mode)
+(require 'hpb-smerge-extend)
+
+(define-key smerge-basic-map (kbd "s") 'smerge-swap)
+(define-key smerge-basic-map (kbd "x") 'hpb-smerge-extend)
+
+(define-key smerge-mode-map (kbd "C-c +") 'hpb-smerge-extend)
+(define-key smerge-mode-map (kbd "C-c !") 'smerge-swap)
+
+(define-key smerge-mode-map (kbd "<f5>") 'hpb-smerge-extend)
+(define-key smerge-mode-map (kbd "<f6>") 'smerge-swap)
 
 (define-key smerge-mode-map (kbd "<f7>") 'smerge-prev)
 (define-key smerge-mode-map (kbd "<f8>") 'smerge-next)
