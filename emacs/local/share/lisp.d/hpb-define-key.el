b9d4ed376c6ed49a66ae8a098e2d8e730dc8f7af
Branislav Zahradník <happy.barney@gmail.com>
2025-10-04 08:26:35 +0200

    [emacs] [hpb-define-key] Silently pass when specified keymap doesn't exist

--- emacs/local/share/lisp.d/hpb-define-key.el
+++ emacs/local/share/lisp.d/hpb-define-key.el
@@ -1,11 +1,14 @@
 ;;; -*- Mode: Emacs-Lisp -*-
 
 (defun hpb-define-key (key-map bindings)
-  "Define bindings in key-map. Bindins is list of (kbd param . symbol).
+	"Define bindings in key-map. Bindins is list of (kbd param . symbol).
 Symbol is eval-ed, so has to be quoted"
 
-  (mapcar (lambda (def) (define-key key-map (kbd (car def)) (eval (cdr def)))) bindings)
-  )
+	(if (boundp key-map)
+		(mapcar (lambda (def) (define-key (symbol-value key-map) (kbd (car def)) (eval (cdr def)))) bindings)
+		(message "hpb-define-key: keymap %s is not defined, skipping bindings" key-map)
+	)
+)
 
 (defun hpb-define-sparse-map (&optional bindings)
   "Define bindings in key-map. Bindins is list of (kbd param . symbol).
