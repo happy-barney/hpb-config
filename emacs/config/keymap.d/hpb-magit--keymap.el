e85b0b2e7c2e13d7171ba5b62ef116afa96e35f5
Branislav Zahradník <happy.barney@gmail.com>
2025-10-09 19:01:22 +0200

    [emacs] [hpb-magit--keymap] Magit keybindings

---
+++ emacs/config/keymap.d/hpb-magit--keymap.el
@@ -0,0 +1,25 @@
+;;; -*- Mode: Emacs-Lisp -*-
+
+;;;###autoload
+(defvar hpb-magit--keymap
+	(hpb-keymap--define-sparse-map
+		'(
+			("g"   . magit-status)
+			("b"   . magit-branch-popup)
+			("y"   . magit-show-refs-popup)
+			("z"   . magit-stash-popup)
+			("s t" . magit-status)
+			("c o" . magit-checkout)
+			("f f" . magit-find-file)
+			("l l" . magit-log-current)
+			(". a" . hpb-magit--stage-current-buffer)
+			(". b" . magit-blame)
+			(". B" . magit-blame-quit)
+			(". e" . magit-ediff-compare)
+			(". E" . magit-ediff-stage)
+			(". l" . magit-log-buffer-file)
+			(". y" . magit-show-refs-popup)
+			(". z" . magit-stash-popup)
+		)
+	)
+)
