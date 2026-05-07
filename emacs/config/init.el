9a246a46a4a627abf1c0f7c12da583e619783316
Branislav Zahradnik <Branislav.Zahradnik@giacom.com>
2026-05-07 10:24:43 +0200

    [emacs] [init] Configure package sources
    - use melpa and stable melpa
    - do not use marmalade anymore

--- emacs/config/init.el
+++ emacs/config/init.el
@@ -21,8 +21,8 @@
 
 ;;; Initialize packages
 (require 'package)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade"    . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa"        . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (package-initialize)
 
