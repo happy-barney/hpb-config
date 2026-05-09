1114e5437cd2f8aa96ea775ce562eca8f026c6aa
Branislav Zahradník <happy.barney@gmail.com>
2026-05-09 14:11:13 +0200

    [emacs] hpb-magit--highlight-markers

--- emacs/config/init.d/init-magit.el
+++ emacs/config/init.d/init-magit.el
@@ -4,5 +4,7 @@
 
 	:hook (
 		(git-commit-setup            . git-commit-turn-on-flyspell)
+		(magit-log-wash-summary      . hpb-magit--highlight-markers)
+		(magit-revision-wash-summary . hpb-magit--highlight-markers)
 	)
 )
