4749654acc9e19ad7cdff63cd9f3a241f23d5e6c
Branislav Zahradnik <Branislav.Zahradnik@giacom.com>
2026-05-12 10:08:37 +0200

    [emacs] hpb-project--switch-buffer

---
+++ emacs/local/share/lisp.d/hpb-project--switch-buffer.el
@@ -0,0 +1,11 @@
+
+;;;###autoload
+(defun hpb-project--switch-buffer ()
+	(interactive)
+	(if (hpb-project--project-root)
+		(projectile-switch-to-buffer)
+		(switch-to-buffer)
+	)
+)
+
+(provide 'hpb-project--switch-buffer)
