80a3c7565b8847d8c3507fdc9bff8a39f3131844
Branislav Zahradník <happy.barney@gmail.com>
2026-05-14 04:12:58 +0200

    [emacs] [init] init-org-mode

---
+++ emacs/config/init.d/init-org-mode.el
@@ -0,0 +1,5 @@
+;;; -*- Mode: Emacs-Lisp -*-
+
+(use-package 'org
+	:ensure nil
+)
--- emacs/config/init.d/org-mode.el
+++
@@ -1,9 +0,0 @@
-;;; -*- Mode: Emacs-Lisp -*-
-
-(require 'org)
-
-(custom-set-variables
- '(org-clock-persist 'history)
- )
-
-(org-clock-persistence-insinuate)
