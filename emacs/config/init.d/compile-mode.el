dc87b30b9c4e1515f7068e1d15b43551dcfdeee6
Branislav Zahradník <happy.barney@gmail.com>
2026-05-15 04:40:49 +0200

    [emacs] [init] init-compile

--- emacs/config/init.d/compile-mode.el
+++
@@ -1,8 +0,0 @@
-
-(require 'ansi-color)
-
-(defun hpb/ansi-colorize-buffer ()
-  (let ((buffer-read-only nil))
-    (ansi-color-apply-on-region (point-min) (point-max))))
-
-(add-hook 'compilation-filter-hook 'hpb/ansi-colorize-buffer)
