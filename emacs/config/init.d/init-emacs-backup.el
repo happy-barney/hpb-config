88dc9532f1fcfe98cf55b0ff46287645c74e1bef
Branislav Zahradník <happy.barney@gmail.com>
2026-05-15 04:39:51 +0200

    [emacs] [init] init-emacs-backup

---
+++ emacs/config/init.d/init-emacs-backup.el
@@ -0,0 +1,13 @@
+
+(use-package emacs
+	:custom
+		(backup-by-copying                  t)
+		(backup-by-copying-when-linked      t)
+		(backup-by-copying-when-mismatch    t)
+		(backup-directory-alist             '((".*" . "~/.cache/emacs/backup.d/")))
+		(delete-old-versions                t)
+		(kept-new-versions                  10)
+		(kept-old-versions                  10)
+		(make-backup-files                  t)
+		(version-control                    t)
+)
