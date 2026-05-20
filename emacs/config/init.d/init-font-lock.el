5d8996f327271141b7a528d974b24936eab6a672
Branislav Zahradník <happy.barney@gmail.com>
2026-05-20 07:36:29 +0200

    [emacs] [init] init-font-lock

---
+++ emacs/config/init.d/init-font-lock.el
@@ -0,0 +1,12 @@
+;;; -*- Mode: Emacs-Lisp -*-
+
+(use-package font-lock
+	:ensure nil
+
+	:custom-face
+		(font-lock-constant-face            ((t (:foreground "dark cyan"))))
+		(font-lock-function-face            ((t (:foreground "RoyalBlue4" :bold t))))
+		(font-lock-keyword-face             ((t (:foreground "dark cyan"))))
+		(font-lock-string-face              ((t (:foreground "forest green"))))
+		(font-lock-variable-face            ((t (:foreground "MediumOrchid4"))))
+)
